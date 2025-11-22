e70ab5a4af4afa2c24d1844b5b88670cc8b3306a
Jerome Haltom <jhaltom@alethic.solutions>
2025-11-22 15:54:11 -0600

    Add DataProtection package and extension methods for using IKeyVaultTokenCredentialProvider. This will allow us to override the key vault token.

--- src/Cogito.Azure.KeyVault.DataProtection/DataProtectionBuilderExtensions.cs
+++ src/Cogito.Azure.KeyVault.DataProtection/DataProtectionBuilderExtensions.cs
@@ -1,16 +1,59 @@
-﻿using Microsoft.AspNetCore.DataProtection;
+﻿using System;
+
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Cogito.Azure.KeyVault.DataProtection
 {
 
+    /// <summary>
+    /// Contains Azure KeyVault-specific extension methods for modifying a <see cref="IDataProtectionBuilder"/> to use the <see cref="IKeyVaultTokenCredentialProvider"/>.
+    /// </summary>
     public static class DataProtectionBuilderExtensions
     {
 
-        public static IDataProtectionBuilder ProtectKeysWithAzureKeyVault(this IDataProtectionBuilder builder)
+        /// <summary>
+        /// Configures the data protection system to protect keys with specified key in Azure KeyVault. This uses the <see cref="IKeyVaultTokenCredentialProvider"/>.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="keyIdentifier"></param>
+        /// <returns></returns>
+        public static IDataProtectionBuilder ProtectKeysWithAzureKeyVault(this IDataProtectionBuilder builder, string keyIdentifier)
         {
-            Argument.AssertNotNull(builder, nameof(builder));
-            Argument.AssertNotNull(keyResolverFactory, nameof(keyResolverFactory));
-            Argument.AssertNotNullOrEmpty(keyIdentifier, nameof(keyIdentifier));
+            return builder.ProtectKeysWithAzureKeyVault(keyIdentifier, p => p.GetRequiredService<IKeyVaultTokenCredentialProvider>().GetCredential());
+        }
+
+        /// <summary>
+        /// Configures the data protection system to protect keys with specified key in Azure KeyVault. This uses the <see cref="IKeyVaultTokenCredentialProvider"/>.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="keyIdentifier"></param>
+        /// <returns></returns>
+        public static IDataProtectionBuilder ProtectKeysWithAzureKeyVault(this IDataProtectionBuilder builder, Uri keyIdentifier)
+        {
+            return builder.ProtectKeysWithAzureKeyVault(keyIdentifier, p => p.GetRequiredService<IKeyVaultTokenCredentialProvider>().GetCredential());
+        }
+
+        /// <summary>
+        /// Configures the data protection system to protect keys with specified key in Azure KeyVault. This uses the <see cref="IKeyVaultTokenCredentialProvider"/>.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="keyIdentifierFactory"></param>
+        /// <returns></returns>
+        public static IDataProtectionBuilder ProtectKeysWithAzureKeyVault(this IDataProtectionBuilder builder, Func<IServiceProvider, string> keyIdentifierFactory)
+        {
+            return builder.ProtectKeysWithAzureKeyVault(keyIdentifierFactory, p => p.GetRequiredService<IKeyVaultTokenCredentialProvider>().GetCredential());
+        }
+
+        /// <summary>
+        /// Configures the data protection system to protect keys with specified key in Azure KeyVault. This uses the <see cref="IKeyVaultTokenCredentialProvider"/>.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="keyIdentifierFactory"></param>
+        /// <returns></returns>
+        public static IDataProtectionBuilder ProtectKeysWithAzureKeyVault(this IDataProtectionBuilder builder, Func<IServiceProvider, Uri> keyIdentifierFactory)
+        {
+            return builder.ProtectKeysWithAzureKeyVault(keyIdentifierFactory, p => p.GetRequiredService<IKeyVaultTokenCredentialProvider>().GetCredential());
         }
 
     }
